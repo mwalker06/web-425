158425786874a51f02b3d393b6959eb763194aa2
mwalker06 <81312304+mwalker06@users.noreply.github.com>
2023-06-04 12:33:21 -0500

    week-2 exercise 2.3 -basic routing

--- week-1/profile-app/src/app/my-image/my-image.component.ts
+++ week-1/profile-app/src/app/my-image/my-image.component.ts
@@ -1,10 +1,21 @@
+/**
+* Title: my-image.component.ts
+* Author: Megan Walker
+* Date: 06/03/2023
+* Description: My image component
+* Source: Professor Krasso, Week 2 Web 425 - Basic Routing
+*/
+
+// Import statement
 import { Component, OnInit } from '@angular/core';
 
+// Component details (selector, templateUrl, styleUrls)
 @Component({
   selector: 'app-my-image',
   templateUrl: './my-image.component.html',
   styleUrls: ['./my-image.component.css']
 })
+// Export class
 export class MyImageComponent implements OnInit {
 
   constructor() { }
