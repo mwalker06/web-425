d5d68787eeecb5326dabd1f5c3ff5720300c0a95
Megan Walker <mwalker@TEC0661DEAPP20.local>
2023-06-05 10:50:07 -0500

    added comments

--- week-1/profile-app/src/app/app.module.ts
+++ week-1/profile-app/src/app/app.module.ts
@@ -1,3 +1,12 @@
+/**
+ * Title: app.module.ts
+ * Author: Megan Walker
+ * Date: 06/03/2023
+ * Description: App module for the application
+ * Source: Professor Krasso, Week 2 Web 425 - Basic Routing
+ */
+
+// Import statements
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
@@ -5,6 +14,7 @@ import { AppComponent } from './app.component';
 import { MyImageComponent } from './my-image/my-image.component';
 import { MyDetailsComponent } from './my-details/my-details.component';
 
+// NgModule decorator
 @NgModule({
   declarations: [
     AppComponent,
@@ -17,4 +27,5 @@ import { MyDetailsComponent } from './my-details/my-details.component';
   providers: [],
   bootstrap: [AppComponent]
 })
+// Export class AppModule
 export class AppModule { }
